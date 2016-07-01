217bd5061c2071aa387d1241bacceb40b5179115
vladimirgor <vladimirgor2011@yandex.ru>
2016-07-01 15:45:05 +0500

    Revert "second"
    This reverts commit b6c08d99b69cf40dde041bedbd7dcd20ac353d8c.

--- theme/comment.php
+++
@@ -1,7 +0,0 @@
-<form method="POST">
-    Your Comment:
-    <br>
-    <textarea name="comment" required ></textarea>
-    <br>
-    <input type="submit" value="Sent your comment" >
-</form>
--- theme/view.php
+++
@@ -1,3 +0,0 @@
-<p><b><?=$title?></b></p>
-<p><?=$content?></p>
-<a href="/article/Show_all">Back</a>
